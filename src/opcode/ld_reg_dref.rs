05eb28d076971a83767ea5d96b03c71c87347fe7
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2019-01-16 00:20:07 +0800

    Implement "ldi_a_hl_dref" and "ldd_a_hl_dref" with tests

--- src/opcode/ld_reg_dref.rs
+++ src/opcode/ld_reg_dref.rs
@@ -29,6 +29,34 @@ ld_reg_dref_fn!(ld_e_hl_dref, (hl) > set_e);
 ld_reg_dref_fn!(ld_h_hl_dref, (hl) > set_h);
 ld_reg_dref_fn!(ld_l_hl_dref, (hl) > set_l);
 
+#[inline]
+pub fn ldi_a_hl_dref(cpu: &mut Cpu) -> (Cycle, OpLength) {
+    ld(
+        cpu,
+        &load_byte_from_reg_dref(&Registers::hl),
+        &store_to_reg(&Registers::set_a),
+    );
+
+    let hl = cpu.registers.hl();
+    cpu.registers.set_hl(hl + 1);
+
+    (Cycle(8), OpLength(1))
+}
+
+#[inline]
+pub fn ldd_a_hl_dref(cpu: &mut Cpu) -> (Cycle, OpLength) {
+    ld(
+        cpu,
+        &load_byte_from_reg_dref(&Registers::hl),
+        &store_to_reg(&Registers::set_a),
+    );
+
+    let hl = cpu.registers.hl();
+    cpu.registers.set_hl(hl - 1);
+
+    (Cycle(8), OpLength(1))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -73,4 +101,54 @@ mod test {
     ld_reg_dref_test!(run_ld_e_hl_dref, ld_e_hl_dref, set_hl, e, set_e);
     ld_reg_dref_test!(run_ld_h_hl_dref, ld_h_hl_dref, set_hl, h, set_h);
     ld_reg_dref_test!(run_ld_l_hl_dref, ld_l_hl_dref, set_hl, l, set_l);
+
+    #[test]
+    fn run_ldi_a_hl_dref() {
+        // Arrange: prepare cpu.
+        let the_addr = 0x0101;
+        let the_value = 0x42;
+
+        let mut init_cpu = Cpu::default();
+        init_cpu.mmu.write_byte(the_addr, the_value).unwrap();
+        init_cpu.registers.set_hl(the_addr as u16);
+
+        let mut modified_cpu = init_cpu.clone();
+
+        // Action.
+        ldi_a_hl_dref(&mut modified_cpu);
+
+        // Assert: check register value.
+        let actual_value = modified_cpu.registers.a();
+        assert_eq!(actual_value, the_value);
+
+        // Assert: other state.
+        init_cpu.registers.set_a(the_value);
+        init_cpu.registers.set_hl((the_addr + 1) as u16);
+        assert!(init_cpu == modified_cpu);
+    }
+
+    #[test]
+    fn run_ldd_a_hl_dref() {
+        // Arrange: prepare cpu.
+        let the_addr = 0x0101;
+        let the_value = 0x42;
+
+        let mut init_cpu = Cpu::default();
+        init_cpu.mmu.write_byte(the_addr, the_value).unwrap();
+        init_cpu.registers.set_hl(the_addr as u16);
+
+        let mut modified_cpu = init_cpu.clone();
+
+        // Action.
+        ldd_a_hl_dref(&mut modified_cpu);
+
+        // Assert: check register value.
+        let actual_value = modified_cpu.registers.a();
+        assert_eq!(actual_value, the_value);
+
+        // Assert: other state.
+        init_cpu.registers.set_a(the_value);
+        init_cpu.registers.set_hl((the_addr - 1) as u16);
+        assert!(init_cpu == modified_cpu);
+    }
 }
--- src/opcode/table.rs
+++ src/opcode/table.rs
@@ -95,6 +95,10 @@ pub fn op_table(op_code: u8) -> OpFn {
         0x6E => &ld_reg_dref::ld_l_hl_dref,
         0x7E => &ld_reg_dref::ld_a_hl_dref,
 
+        // ldh a hl dref
+        0x2A => &ld_reg_dref::ldi_a_hl_dref,
+        0x3A => &ld_reg_dref::ldd_a_hl_dref,
+
         _ => &unimplement_op_fn,
     }
 }
