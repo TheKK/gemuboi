bee9b5762daee56b2c0d0e6b900e7572d7307416
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2019-02-15 02:23:39 +0800

    Change add_a_reg series instruction and test into macro

--- src/opcode/d8_arithmetic_logical.rs
+++ src/opcode/d8_arithmetic_logical.rs
@@ -9,105 +9,120 @@ fn carry_add_u8(l: u8, r: u8) -> (u8, bool, bool) {
     (val, half_carry, carry)
 }
 
-// Z 0 H C
-pub fn add_a_b(cpu: &mut Cpu) -> InstructionResult {
-    let a = cpu.registers.a();
-    let b = cpu.registers.b();
+macro_rules! add_a_instruction {
+    ($ins_name: ident, $from: ident) => {
+        // Z 0 H C
+        pub fn $ins_name(cpu: &mut Cpu) -> InstructionResult {
+            let a = cpu.registers.a();
+            let val = cpu.registers.$from();
 
-    let (result, half_carry, carry) = carry_add_u8(a, b);
+            let (result, half_carry, carry) = carry_add_u8(a, val);
 
-    cpu.registers.set_a(result);
+            cpu.registers.set_a(result);
 
-    cpu.registers.flag.set_zero(result == 0);
-    cpu.registers.flag.set_sub(false);
-    cpu.registers.flag.set_half_carry(half_carry);
-    cpu.registers.flag.set_carry(carry);
+            cpu.registers.flag.set_zero(result == 0);
+            cpu.registers.flag.set_sub(false);
+            cpu.registers.flag.set_half_carry(half_carry);
+            cpu.registers.flag.set_carry(carry);
 
-    (Cycle(4), OpLength(1))
+            (Cycle(4), OpLength(1))
+        }
+    };
 }
+
+add_a_instruction!(add_a_b, b);
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
-    use crate::cpu::Cpu;
+    macro_rules! test_add_a_instruction {
+        ($inst_name: ident, $reg_setter: ident) => {
+            mod $inst_name {
+                use super::super::*;
 
-    #[test]
-    fn run_add_a_b_zero() {
-        let reg_a = 0b00000000;
-        let reg_b = 0b00000000;
+                use crate::cpu::Cpu;
 
-        let mut actual_cpu = Cpu::default();
-        actual_cpu.registers.set_a(reg_a);
-        actual_cpu.registers.set_b(reg_b);
+                fn run_test(with_carry: bool, with_half_carry: bool) {
+                    let (reg_a, reg_other) = match (with_carry, with_half_carry) {
+                        (false, false) => (0b00000001_u8, 0b00000001_u8),
+                        (true, false) => (0b10000001, 0b10000001),
+                        (false, true) => (0b00001001, 0b00001001),
+                        (true, true) => (0b10001001, 0b10001001),
+                    };
 
-        actual_cpu.registers.flag.set_zero(false);
-        actual_cpu.registers.flag.set_sub(true);
-        actual_cpu.registers.flag.set_carry(true);
-        actual_cpu.registers.flag.set_half_carry(true);
+                    let (result, _) = reg_a.overflowing_add(reg_other);
 
-        let mut expected_cpu = actual_cpu.clone();
-        expected_cpu.registers.set_a(reg_a + reg_b);
+                    let mut actual_cpu = Cpu::default();
+                    actual_cpu.registers.set_a(reg_a);
+                    actual_cpu.registers.$reg_setter(reg_other);
 
-        expected_cpu.registers.flag.set_zero(true);
-        expected_cpu.registers.flag.set_sub(false);
-        expected_cpu.registers.flag.set_carry(false);
-        expected_cpu.registers.flag.set_half_carry(false);
+                    actual_cpu.registers.flag.set_zero(true);
+                    actual_cpu.registers.flag.set_sub(true);
+                    actual_cpu.registers.flag.set_carry(!with_carry);
+                    actual_cpu.registers.flag.set_half_carry(!with_half_carry);
 
-        add_a_b(&mut actual_cpu);
+                    let mut expected_cpu = actual_cpu.clone();
+                    expected_cpu.registers.set_a(result);
 
-        assert_eq!(actual_cpu, expected_cpu);
-    }
+                    expected_cpu.registers.flag.set_zero(false);
+                    expected_cpu.registers.flag.set_sub(false);
+                    expected_cpu.registers.flag.set_carry(with_carry);
+                    expected_cpu.registers.flag.set_half_carry(with_half_carry);
 
-    fn run_add_a_b(with_carry: bool, with_half_carry: bool) {
-        let (reg_a, reg_b) = match (with_carry, with_half_carry) {
-            (false, false) => (0b00000001_u8, 0b00000001_u8),
-            (true, false) => (0b10000001, 0b10000001),
-            (false, true) => (0b00001001, 0b00001001),
-            (true, true) => (0b10001001, 0b10001001),
+                    $inst_name(&mut actual_cpu);
+
+                    assert_eq!(actual_cpu, expected_cpu);
+                }
+
+                #[test]
+                fn run_test_zero() {
+                    let reg_a = 0b00000000;
+                    let reg_other = 0b00000000;
+
+                    let mut actual_cpu = Cpu::default();
+                    actual_cpu.registers.set_a(reg_a);
+                    actual_cpu.registers.$reg_setter(reg_other);
+
+                    actual_cpu.registers.flag.set_zero(false);
+                    actual_cpu.registers.flag.set_sub(true);
+                    actual_cpu.registers.flag.set_carry(true);
+                    actual_cpu.registers.flag.set_half_carry(true);
+
+                    let mut expected_cpu = actual_cpu.clone();
+                    expected_cpu.registers.set_a(reg_a + reg_other);
+
+                    expected_cpu.registers.flag.set_zero(true);
+                    expected_cpu.registers.flag.set_sub(false);
+                    expected_cpu.registers.flag.set_carry(false);
+                    expected_cpu.registers.flag.set_half_carry(false);
+
+                    $inst_name(&mut actual_cpu);
+
+                    assert_eq!(actual_cpu, expected_cpu);
+                }
+
+                #[test]
+                fn run_test_without_carry_without_half_carry() {
+                    run_test(false, false);
+                }
+
+                #[test]
+                fn run_test_with_carry_with_half_carry() {
+                    run_test(true, true);
+                }
+
+                #[test]
+                fn run_test_with_carry_without_half_carry() {
+                    run_test(true, false);
+                }
+
+                #[test]
+                fn run_test_without_carry_with_half_carry() {
+                    run_test(false, true);
+                }
+            }
         };
-
-        let (result, _) = reg_a.overflowing_add(reg_b);
-
-        let mut actual_cpu = Cpu::default();
-        actual_cpu.registers.set_a(reg_a);
-        actual_cpu.registers.set_b(reg_b);
-
-        actual_cpu.registers.flag.set_zero(true);
-        actual_cpu.registers.flag.set_sub(true);
-        actual_cpu.registers.flag.set_carry(!with_carry);
-        actual_cpu.registers.flag.set_half_carry(!with_half_carry);
-
-        let mut expected_cpu = actual_cpu.clone();
-        expected_cpu.registers.set_a(result);
-
-        expected_cpu.registers.flag.set_zero(false);
-        expected_cpu.registers.flag.set_sub(false);
-        expected_cpu.registers.flag.set_carry(with_carry);
-        expected_cpu.registers.flag.set_half_carry(with_half_carry);
-
-        add_a_b(&mut actual_cpu);
-
-        assert_eq!(actual_cpu, expected_cpu);
     }
 
-    #[test]
-    fn run_add_a_b_without_carry_without_half_carry() {
-        run_add_a_b(false, false);
-    }
-
-    #[test]
-    fn run_add_a_b_with_carry_with_half_carry() {
-        run_add_a_b(true, true);
-    }
-
-    #[test]
-    fn run_add_a_b_with_carry_without_half_carry() {
-        run_add_a_b(true, false);
-    }
-
-    #[test]
-    fn run_add_a_b_without_carry_with_half_carry() {
-        run_add_a_b(false, true);
-    }
+    test_add_a_instruction!(add_a_b, set_b);
 }
