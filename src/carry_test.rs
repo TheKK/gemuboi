0a6ef44621d43b96f07ab7c9a7af8fe4ad4746ab
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2019-02-17 17:13:19 +0800

    Add new 'carry_add' into CarryTest trait

--- src/carry_test.rs
+++ src/carry_test.rs
@@ -5,10 +5,22 @@ pub struct CarryTestResult<T> {
 }
 
 pub trait CarryTest: Sized {
+    fn carry_add(self, rhs: Self) -> CarryTestResult<Self>;
     fn carry_sub(self, rhs: Self) -> CarryTestResult<Self>;
 }
 
 impl CarryTest for u8 {
+    fn carry_add(self, rhs: Self) -> CarryTestResult<Self> {
+        let (val, carry) = self.overflowing_add(rhs);
+        let (_, half_carry) = (self << 4).overflowing_add(rhs << 4);
+
+        CarryTestResult {
+            val,
+            half_carry,
+            carry,
+        }
+    }
+
     fn carry_sub(self, rhs: Self) -> CarryTestResult<Self> {
         let (val, carry) = self.overflowing_sub(rhs);
         let (_, half_carry) = (self & 0xF).overflowing_sub(rhs & 0xF);
