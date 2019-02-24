bfaaefe14aeb763a178bf212e155a0241a696882
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2019-02-24 20:18:45 +0800

    Add read_byte_argument method for Cpu

--- src/cpu.rs
+++ src/cpu.rs
@@ -14,4 +14,45 @@ impl Cpu {
 
         cycle
     }
+
+    pub fn read_byte_argument(&self, index: u16) -> u8 {
+        let arg_addr = self.registers.pc().saturating_add(index);
+
+        self.mmu.read_byte(arg_addr)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Cpu;
+
+    #[test]
+    fn read_byte_argument_normal() {
+        let pc = 0x42;
+
+        let arg_index = 1;
+        let arg_value = 0x12;
+
+        let mut cpu = Cpu::default();
+        cpu.mmu.write_byte(pc + arg_index, arg_value).unwrap();
+        cpu.registers.set_pc(pc);
+
+        assert_eq!(arg_value, cpu.read_byte_argument(arg_index));
+    }
+
+    #[test]
+    fn read_byte_argument_out_of_bound() {
+        use crate::mmu::INVALID_READ_DEFAULT_VALUE;
+
+        let pc = 0x10;
+        let arg_index = 0xFFFF;
+
+        let mut cpu = Cpu::default();
+        cpu.registers.set_pc(pc);
+
+        assert_eq!(
+            cpu.read_byte_argument(arg_index),
+            INVALID_READ_DEFAULT_VALUE
+        );
+    }
 }
