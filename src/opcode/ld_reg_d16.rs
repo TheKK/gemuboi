55e79c1a7f6a3180c7c70f02302adcb8fa8eef4b
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2019-03-27 23:56:03 +0800

    Implement ld_reg_d16 for de, hl and sp with tests

--- src/opcode/ld_reg_d16.rs
+++ src/opcode/ld_reg_d16.rs
@@ -3,15 +3,27 @@ use crate::opcode::table::{Cycle, OpLength};
 use crate::opcode::types::InstructionResult;
 use crate::registers::Registers;
 
+pub fn ld_bc_d16(cpu: &mut Cpu) -> InstructionResult {
+    ld_reg_d16(cpu, &Registers::set_bc)
+}
+
+pub fn ld_de_d16(cpu: &mut Cpu) -> InstructionResult {
+    ld_reg_d16(cpu, &Registers::set_de)
+}
+
+pub fn ld_hl_d16(cpu: &mut Cpu) -> InstructionResult {
+    ld_reg_d16(cpu, &Registers::set_hl)
+}
+
+pub fn ld_sp_d16(cpu: &mut Cpu) -> InstructionResult {
+    ld_reg_d16(cpu, &Registers::set_sp)
+}
+
 fn ld_reg_d16(cpu: &mut Cpu, reg_setter: &Fn(&mut Registers, u16)) -> InstructionResult {
     let d16 = cpu.read_word_argument(1);
     reg_setter(&mut cpu.registers, d16);
 
     (Cycle(12), OpLength(3))
-}
-
-pub fn ld_bc_d16(cpu: &mut Cpu) -> InstructionResult {
-    ld_reg_d16(cpu, &Registers::set_bc)
 }
 
 #[cfg(test)]
@@ -37,11 +49,14 @@ mod test {
 
                     $inst(&mut actual_cpu);
 
-                    assert_eq!(actual_cpu.registers, expected_cpu.registers);
+                    assert_eq!(actual_cpu, expected_cpu);
                 }
             }
         };
     }
 
     test_ld_reg_d16!(ld_bc_d16, set_bc);
+    test_ld_reg_d16!(ld_de_d16, set_de);
+    test_ld_reg_d16!(ld_hl_d16, set_hl);
+    test_ld_reg_d16!(ld_sp_d16, set_sp);
 }
